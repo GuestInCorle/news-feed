d6e3721d03dd760acf6728fe104ac33363e2c689
Nico Cross <quqtus@gmail.com>
2020-09-16 10:41:00 +0700

    Feature: Integrate notifications on Android and iOS

---
+++ index.ts
@@ -0,0 +1,43 @@
+import 'react-native-gesture-handler';
+import {registerRootComponent} from 'expo';
+
+import App from './src/App';
+import {
+  scheduleNotificationAsync,
+  setNotificationHandler,
+} from 'expo-notifications';
+import database from './src/database';
+import {Image, Platform} from 'react-native';
+
+if (Platform.OS === 'android' || Platform.OS === 'ios') {
+  setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+
+  const index = Math.floor(Math.random() * database.articles.length);
+  const article = database.articles[index];
+  // noinspection JSIgnoredPromiseFromCall
+  scheduleNotificationAsync({
+    identifier: String(article.id),
+    content: {
+      title: article.title,
+      body: 'Нажмите, чтобы перейти к новости',
+      data: {
+        url: `news-feed://article/${article.id}`,
+      },
+      attachments: [{url: Image.resolveAssetSource(article.image).uri}],
+    },
+    trigger: {
+      seconds: 10,
+    },
+  });
+}
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React, {FC} from 'react';
-import {Platform, StyleSheet} from 'react-native';
+import React, {FC, useCallback, useEffect, useState} from 'react';
+import {Linking, Platform, StyleSheet} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator, HeaderBackButton} from '@react-navigation/stack';
 import ArticleListScreen from './ArticleListScreen';
 import ArticleScreen from './ArticleScreen';
 import {LinkingOptions} from '@react-navigation/native/lib/typescript/src/types';
+import {addNotificationResponseReceivedListener} from 'expo-notifications';
 
 export type RootStackParamList = {
   ArticleList: undefined;
@@ -37,42 +38,62 @@ const linking = {
   },
 } as LinkingOptions;
 
-const App: FC = () => (
-  <>
-    <StatusBar style="auto" />
-    <NavigationContainer linking={linking}>
-      <Stack.Navigator mode="modal" initialRouteName="ArticleList">
-        <Stack.Screen
-          name="ArticleList"
-          options={{title: 'Новости'}}
-          component={ArticleListScreen}
-        />
-        <Stack.Screen
-          name="Article"
-          options={({navigation}) => ({
-            title: 'Новость',
-            headerShown: Platform.OS !== 'web',
-            cardStyle: styles.card,
-            headerLeft: (props) => (
-              // Yes, there is no useCallback here. It's normal.
-              <HeaderBackButton
-                {...props}
-                onPress={() => {
-                  if (navigation.canGoBack()) {
-                    navigation.goBack();
-                  } else {
-                    navigation.replace('ArticleList');
-                  }
-                }}
-              />
-            ),
-          })}
-          component={ArticleScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </>
-);
+const App: FC = () => {
+  const [isReady, setReady] = useState(false);
+  const [lastUrl, setLastUrl] = useState('');
+  const onReady = useCallback(() => setReady(true), []);
+  useEffect(() => {
+    const subscription = addNotificationResponseReceivedListener((response) => {
+      const url = response.notification.request.content.data.url;
+      if (typeof url === 'string' && url.startsWith('news-feed://')) {
+        setLastUrl(url);
+      }
+    });
+    return () => subscription.remove();
+  }, []);
+  useEffect(() => {
+    if (isReady && lastUrl) {
+      // noinspection JSIgnoredPromiseFromCall
+      Linking.openURL(lastUrl);
+    }
+  }, [isReady, lastUrl]);
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer linking={linking} onReady={onReady}>
+        <Stack.Navigator mode="modal" initialRouteName="ArticleList">
+          <Stack.Screen
+            name="ArticleList"
+            options={{title: 'Новости'}}
+            component={ArticleListScreen}
+          />
+          <Stack.Screen
+            name="Article"
+            options={({navigation}) => ({
+              title: 'Новость',
+              headerShown: Platform.OS !== 'web',
+              cardStyle: styles.card,
+              headerLeft: (props) => (
+                // Yes, there is no useCallback here. It's normal.
+                <HeaderBackButton
+                  {...props}
+                  onPress={() => {
+                    if (navigation.canGoBack()) {
+                      navigation.goBack();
+                    } else {
+                      navigation.replace('ArticleList');
+                    }
+                  }}
+                />
+              ),
+            })}
+            component={ArticleScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
 
 export default App;
 
